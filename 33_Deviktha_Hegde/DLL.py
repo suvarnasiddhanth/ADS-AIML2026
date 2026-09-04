fa7a845e4efd88a215fd57ba17ecb2d9c14e09eb
Dh-2004 <devikthahegde2004@gmail.com>
2026-09-04 09:03:54 +0530

    files update

--- 33_Deviktha_Hegde/DLL.py
+++ 33_Deviktha_Hegde/DLL.py
@@ -1,73 +1,122 @@
-#double linked list
 class Node:
-  def __init__(self, data):
-    self.data = data
-    self.prev = None
-    self.next = None
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
 
 class DoublyLinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-#Forward Display
-def display_fwd(self):
-  if self.head is None:
-    print('List is empty')
-    return
-  cur = self.head
-  while cur is not None:
-    print(cur.data)
-    cur=cur.next
+    # Insert at beginning
+    def insert_beginning(self, data):
+        new_node = Node(data)
 
-  print('None')
+        if self.head is None:
+            self.head = self.tail = new_node
+            return
 
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
 
-#Backward Display
-def display_back(self):
-  if self.head is None:
-    print('List is empty')
-    return
-  cur=self.tail
-  while cur is not None:
-    print(cur.data)
-    cur=cur.prev
+    # Insert at end
+    def insert_end(self, data):
+        new_node = Node(data)
 
-  print('None')
+        if self.head is None:
+            self.head = self.tail = new_node
+            return
 
-#Insert at beginning
-def insert_beginning(self, data):
-  newnode=Node(data)
+        new_node.prev = self.tail
+        self.tail.next = new_node
+        self.tail = new_node
 
-  if self.head is None:
-    self.head = newnode
-    self.tail = newnode
-  else:
-    newnode.next = self.head
-    self.head.prev = newnode
-    self.head = newnode
+    # Display forward
+    def display_forward(self):
+        current = self.head
 
+        while current:
+            print(current.data, end=" <-> ")
+            current = current.next
 
-#Insert at the End
-def insert_end(self,data):
-  newnode=Node(data)
-  if self.head is None:
-    self.head=newnode
-    self.tail=newnode
-  else:
-    self.tail.next=newnode
-    newnode.prev=self.tail
-    self.tail=newnode
+        print("None")
 
+    # Display backward
+    def display_backward(self):
+        current = self.tail
 
-#Insert at position
-def insert_position(self,data,position):
-  newnode=Node(data)
+        while current:
+            print(current.data, end=" <-> ")
+            current = current.prev
 
-  if position == 0:
-    self.insert_beginning(data)
-    return
+        print("None")
 
+    # Delete from beginning
+    def delete_beginning(self):
+        if self.head is None:
+            print("List is empty")
+            return
 
+        if self.head == self.tail:
+            self.head = self.tail = None
+            return
 
-    cur=self.head
+        self.head = self.head.next
+        self.head.prev = None
+
+    # Delete from end
+    def delete_end(self):
+        if self.tail is None:
+            print("List is empty")
+            return
+
+        if self.head == self.tail:
+            self.head = self.tail = None
+            return
+
+        self.tail = self.tail.prev
+        self.tail.next = None
+
+    # Search
+    def search(self, data):
+        current = self.head
+
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+
+        return False
+
+    # Length
+    def length(self):
+        count = 0
+        current = self.head
+
+        while current:
+            count += 1
+            current = current.next
+
+        return count
+
+    # Reverse
+    def reverse(self):
+        current = self.head
+
+        while current:
+            current.next, current.prev = current.prev, current.next
+            current = current.prev
+
+        self.head, self.tail = self.tail, self.head
+        
+        
+dll = DoublyLinkedList()
+
+dll.insert_end(10)
+dll.insert_end(20)
+dll.insert_end(30)
+
+dll.display_forward()
