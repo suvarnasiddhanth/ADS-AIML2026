a91a1399fcc0f5a2f0a6366cebe7d7fe17205683
ujwalvk <159772908+ujwalvk@users.noreply.github.com>
2026-09-18 11:35:16 +0530

    Enhance stack implementation with menu-driven format
    Refactored stack implementation to include user interaction for stack operations.

--- 03_ujwal_kulkarni/stack_implementation.py
+++ 03_ujwal_kulkarni/stack_implementation.py
@@ -17,15 +17,50 @@ class stack:
         return len(self.stack)
     def isEmpty(self):
         return len(self.stack) == 0
-s=stack()
-s.push(1)
-s.push(2)
-s.push(3)
+myStack = Stack()
 
+m = "1"
 
-print("Stack: ", s.stack)
-print("Pop: ", s.pop())
-print("Stack after Pop: ", s.stack)
-print("Peek: ", s.peek())
-print("isEmpty: ", s.isEmpty())
-print("Size: ", s.size())
+while(m == "1"):
+    print("\n1. Push\n2. Pop\n3. Peek\n4. IsEmpty\n5. Size\n6. Print Stack")
+    choice = int(input("Enter your choice : "))
+
+    match choice:
+
+        case 1:
+            element = int(input("Enter an element to insert into Stack : "))
+            myStack.push(element)
+
+        case 2:
+            if len(myStack.stack) == 0:
+                print("Stack is Empty.")
+            else:
+                print("Deleted element from Stack : ", myStack.pop())
+
+        case 3:
+            if len(myStack.stack) == 0:
+                print("Stack is Empty.")
+            else:
+                print("Peek element of Stack : ", myStack.peek())
+
+        case 4:
+            print("Stack is Empty : ", myStack.isEmpty())
+
+        case 5:
+            print("Size of Stack : ", myStack.size())
+
+        case 6:
+            print("Stack : ", myStack.stack)
+
+        case _:
+            print("Invalid choice.")
+
+    while True:
+        m = input("Do you want to continue operation (Yes = 1 / No = 0) : ")
+
+        if m in ("1", "0"):
+            break
+        else:
+            print("Wrong Choice, Please enter only 1 and 0.")
+
+print("Program exited Successfully.")
